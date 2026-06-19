ec042a3ddea6288d9403477439daa91a35109372
Shivanshu80 <shivamsoni11001@gmail.com>
2026-06-19 14:05:02 +0530

    dto files changed

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/userDto';
+import { CreateUserDto } from './dto/userdto';
 
 @Controller('user')
 export class UserController {
